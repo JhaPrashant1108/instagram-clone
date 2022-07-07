ec2cf7d2f8105a01f43dea578ffe4543b5671010
jhaprashant1108 <prashant.jha0008@gmail.com>
2022-07-07 23:25:26 +0530

    search bar added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './styles/Header.css'
+import { NavLink } from 'react-router-dom'
+import { FaSearch } from 'react-icons/fa'
 
 function Header() {
+
   return (
-    <div className='header'>Header</div>
+    <div className='header'>
+      <div className='header-container'>
+        <NavLink className='header-logo' to={'/'} >InstaClone</NavLink>
+        <div className='header-searchbar' >
+          <form>
+            <FaSearch style={ {color: "rgb(197, 197, 197)", fontSize: "1rem" , fontWeight:"100" } } />
+            <input type="text" placeholder='Search' />
+          </form>
+        </div>
+
+      </div>
+
+
+    </div>
   )
 }
 
